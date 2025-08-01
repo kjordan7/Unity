10cb9d44b6008923ac485b7d6a18fa9fbf157867
kjordan7 <kalexjordan@gmail.com>
2025-08-01 11:13:50 -0400

    Add files via upload

---
+++ JumpMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpMovement : MonoBehaviour
+{
+
+    float jumpHeight = 15f;
+    Rigidbody playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        playerRB = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Jump")){
+
+
+            playerRB.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        }
+    }
+}

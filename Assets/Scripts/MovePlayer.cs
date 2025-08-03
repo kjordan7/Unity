afe4e29692753202a32215e8e45e55fa7e86a503
kjordan7 <kalexjordan@gmail.com>
2025-08-02 21:48:00 -0400

    Update MovePlayer.cs

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -4,61 +4,55 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-//information before we start:
-
-//create variables
-//variables help us store, manipulate and refer to info
-//have names, contain data, and is a particular type
-
-//example int hitPoints 20;
-//float speed 3.8f;
-//bool isAlive = true;
-//string myName = Jordan;
-
-    //[SerializeField]float xValue = 0f;
-   
-    //[SerializeField]float zValue = 0f;
-// variable make it easy to find and easy to manipulate
+  //information and comments in green with two slashes(//).
 
 
-    // Start is called before the first frame update (happens once)
+  //variables help us store, manipulate and refer to information in Unity Engine.
 
-    [SerializeField]float playerSpeed = 10f;
-    //methods = functions
-    void Start()
-    {   
-        PrintInstructions();
-        
-        //accessing the transform on the game object -transform
-        //Translate - Unity built in to have an gameObject to move
-        //transform.Translate (1,0,0);
-        //this will move object one unity unit on the X axis
+  //Examples:
+  //int hitPoints = 20;
+  //float speed = 3.8f;
+  //bool isAlive = true;
+  //string myName = Jordan;
+
+
+  //create variables here:
+
+  [SerializeField] float playerSpeed = 10f;
+  //Creatng a float variable (numbers with decimals) calledplayerSpeed and setting it to 10 units.
+  // [SerializeField] makes variables easy to find and manipulate in Unity Editor.
+
+  public string playerInstructionsOne;
+  public string playerInstructionsTwo;
+  // creating open text strings to give instructions to players.
+  
+
+  void Start()
+  //void = functions
+  // Start is called before the first frame update (happens once in the script).
+  {
+
+    //set text variables  to Introduce players to the game.
+    playerInstructionsOne = "Welcome to the Game!";
+    playerInstructionsTwo = "Move your player with WASD or Arrow Keys!";
+
+    //Print text variables to the output console in Unity.
+    Debug.Log(playerInstructionsOne);
+    Debug.Log(playerInstructionsTwo);
     }
 
-    // Update is called once per frame (every time (tick) the script is alive)
-    // this will move object .01 unity unit on the x axis continously
-    //time.DeltaTime= use info on computer to play the same on frame independent
-    void Update()
-    {
-        
-        PlayerMovement();
-    }
-    void PrintInstructions()
-    {
 
-        Debug.Log("Welcome to the Game.");
-        Debug.Log("Move your player with WASD or Arrow Keys");
-        Debug.Log("Don't hit the wall!");
+  // Update is called once per frame (every time (tick) the script is alive)
+  void Update()
+  {
+    //set two local variables to move in four directions: up, down, left, and right
+    //with the speed of the player and syncing frames for different devices.
+    float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+    float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
+
+    //move the player on the X and Z axes using the input variables.
+    transform.Translate(moveX, 0, moveZ);
     
 
-    }
-
-    void PlayerMovement(){
-
-        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
-        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;  
-        transform.Translate (xValue, 0, zValue);
-
-    }
-
+  }
 }

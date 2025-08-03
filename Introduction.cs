9a753ac84464af2ddf0d81cd5ad18a3601b9e41c
kjordan7 <kalexjordan@gmail.com>
2025-08-02 21:59:25 -0400

    Update Introduction.cs

--- Introduction.cs
+++ Introduction.cs
@@ -2,41 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovePlayer : MonoBehaviour
+public class IntroductionScript : MonoBehaviour
 {
+    //Variables are entered here
+    public string myIntroduction;
 
-    [SerializeField]float moveX;
-    string myIntroduction;
-    string myQuote;
-    float moveY;
-    float moveZ;
-    bool myChoice = true;
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        myIntroduction = "My name is Jordan";
-        myQuote = "Puyo!";
-        moveX = 5f;
-        moveY = 0;
-        moveZ = 0;
-        myChoice = false;
-        
-        
-        Debug.Log(myIntroduction);
-        Debug.Log(myQuote);
+  //creating a string variable myIntroduction.
+    [SerializeField] public string myQuote;
+
+    //creating a string variable myQuote.
+    [SerializeField] public int oneInteger;
+
+    //creating an integer variable oneInteger.
+    [SerializeField] public float oneFloat;
+
+//creating a float(number with decimals) oneFloat.
+  public bool chooseWisely;
+
+  //creating a boolean variable chooseWisely.
 
 
 
+  //Function Start is called before the first frame update
+  void Start()
+  {
+
+    //setting variables to their data:
+    //string to characters and words.
+    //numbers to number data (Note: an f must be placed wen setting float variables so that 
+    // Unity knows it is a float variable).
+    //boolean to either true or false.
+    myIntroduction = "My name is Jordan";
+    myQuote = "Puyo!";
+    chooseWisely = true;
+    oneInteger = 1;
+    oneFloat = 10.25f;
 
 
+    //This will print each variable to the console window once
+    Debug.Log(myIntroduction);
+    Debug.Log(myQuote);
+    Debug.Log(chooseWisely);
+    Debug.Log(oneInteger);
+    Debug.Log(oneFloat);
 
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(moveX * Time.deltaTime, moveY, moveZ);
-        
-    }
+
+    //Moves the sphere 1 unit  on the Z axis once.
+    //transform.Translate(0,0,onefloat);
+  }
+
+  // Update is called once per frame (happens as long as the script is alive).
+  void Update()
+  {
+
+    //Moves the sphere 10.25 units on the Z axis continuously.
+    //transform.Translate(0,0,oneInteger);
+  }
 }
+
